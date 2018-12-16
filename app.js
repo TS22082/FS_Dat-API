309b2109602aa306d312a05c442ce554c9e76acd
GeekCentric <ts22082@gmail.com>
2018-12-16 08:34:37 -0800

    all crud functionalities from fs node module

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var fs = require('fs')
 var Dat = require('dat-node')
+const readline = require('readline')
 
-Dat('/', function (err, dat) {
-  if (err) throw err
-  dat.importFiles()
-  dat.joinNetwork()
-  console.log('My Dat link is: dat://', dat.key.toString('hex'))
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 })
 
+// rl.question('Specify path for dat creation: ', (path) => {
+//   Dat(path, function (err, dat) {
+//     if (err) throw err
+//     dat.importFiles()
+//     dat.joinNetwork()
+//     console.log('My Dat link is: dat://', dat.key.toString('hex'))
+//     console.log(`Initiated DAT in file location: ${path}`);
+//   })
+//   rl.close();
+// });
+
+
+//read
 fs.readFile('hello.txt', function(err, data){
   if(err){
     console.log(err)
@@ -16,6 +28,26 @@ fs.readFile('hello.txt', function(err, data){
   }
 })
 
-var data = fs.readFileSync('hello.txt')
-console.log('synced data is: ' + data.toString())
-console.log('end sync')
+///add to file
+fs.appendFile('hello.txt', ' more text.', function (err) {
+  if (err) throw err;
+  console.log('Updated!');
+});
+
+//replcae context
+fs.writeFile('hello.txt', 'This is my text', function (err) {
+  if (err) throw err;
+  console.log('Replaced!');
+});
+
+//rename
+fs.rename('hello.txt', 'delete.txt', function (err) {
+  if (err) throw err;
+  console.log('File Renamed!');
+});
+
+//delete
+fs.unlink('delete.txt', function (err) {
+  if (err) throw err;
+  console.log('File deleted!');
+});
