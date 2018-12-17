74203f24e732dc410a93e8f959369ba3907b5aa2
GeekCentric <ts22082@gmail.com>
2018-12-16 18:15:07 -0800

    all crud functionalities

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var fs = require('fs')
 var Dat = require('dat-node')
 const readline = require('readline')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+
+// Creates dat stream in specified folder
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
 
 // rl.question('Specify path for dat creation: ', (path) => {
 //   Dat(path, function (err, dat) {
@@ -19,7 +21,7 @@ const rl = readline.createInterface({
 // });
 
 
-//read
+//read file
 fs.readFile('hello.txt', function(err, data){
   if(err){
     console.log(err)
@@ -34,7 +36,7 @@ fs.appendFile('hello.txt', ' more text.', function (err) {
   console.log('Updated!');
 });
 
-//replcae context
+//replace context
 fs.writeFile('hello.txt', 'This is my text', function (err) {
   if (err) throw err;
   console.log('Replaced!');
