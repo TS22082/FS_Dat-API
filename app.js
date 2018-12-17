bcf484db3356aa963c8ca4c5678c2d882586a921
GeekCentric <ts22082@gmail.com>
2018-12-17 14:48:58 -0800

    use throw syntax

--- app.js
+++ app.js
@@ -2,22 +2,17 @@ var fs = require('fs')
 var Dat = require('dat-node')
 const readline = require('readline')
 
-
-// Creates dat stream in specified folder
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// })
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 rl.question('Specify path for dat creation: ', (path) => {
   Dat(path, function (err, dat) {
     if (err) throw err
     try {
       dat.importFiles(function (err) {
-        if (err) {
-          console.log(err)
-          console.log('most likely already have a dat stream initiated')
-        }
+        if (err) throw err
       })
       dat.joinNetwork()
       console.log('My Dat link is: dat://', dat.key.toString('hex'))
@@ -29,36 +24,3 @@ rl.question('Specify path for dat creation: ', (path) => {
   rl.close()
 })
 
-
-//read file
-fs.readFile('hello.txt', function(err, data){
-  if(err){
-    console.log(err)
-  } else {
-    console.log('data is: ' + data.toString())
-  }
-})
-
-///add to file
-fs.appendFile('hello.txt', ' more text.', function (err) {
-  if (err) throw err;
-  console.log('Updated!');
-});
-
-//replace context
-fs.writeFile('hello.txt', 'This is my text', function (err) {
-  if (err) throw err;
-  console.log('Replaced!');
-});
-
-//rename
-fs.rename('hello.txt', 'delete.txt', function (err) {
-  if (err) throw err;
-  console.log('File Renamed!');
-});
-
-//delete
-fs.unlink('delete.txt', function (err) {
-  if (err) throw err;
-  console.log('File deleted!');
-});
